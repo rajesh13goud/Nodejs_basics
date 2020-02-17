2a0a6273bfd390b869dc41d2f71388a124a10f8d
rajesh13goud <rajeshdgoud@gmail.com>
2020-02-17 05:44:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+  // console.log(req.headers);
+  const url = req.url;
+  if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
+    res.write('</html>');
+    return res.end()
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('Hello');
+  res.end();
+})
+server.listen(3000);
