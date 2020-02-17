49a2561f2e3d2f52e119046331c0b92cec82756e
rajesh13goud <rajeshdgoud@gmail.com>
2020-02-17 05:56:53 +0000

    Redirecting Requests

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
   // console.log(req.headers);
   const url = req.url;
+  const method = req.method;
   //routing requests
   if(url === '/'){
     res.write('<html>');
@@ -11,6 +13,14 @@ const server = http.createServer((req,res)=>{
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
     res.write('</html>');
     return res.end()
+  }
+  
+  //redircting Requests
+  if(url === '/message' && method === 'POST'){
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode=302;
+    res.setHeader('Location', '/');
+    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('Hello');
